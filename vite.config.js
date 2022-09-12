795f3fc1dcec9b8e6c293c2d2ce489d214b1377b
Ruulul <vamjunior01@gmail.com>
2022-09-12 09:05:03 -0300

    funky bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            'utils': '$lib/utils/utils.js'
+        }
+    },
     server: {
         port: 4127,
         hmr: {
