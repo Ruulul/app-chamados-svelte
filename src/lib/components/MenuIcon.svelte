09480ff5a42d34ed20a1c51701bb4a4fa69cbfd8
Ruulul <vamjunior01@gmail.com>
2022-09-12 10:10:54 -0300

    "outlined" icons

--- src/lib/components/MenuIcon.svelte
+++ src/lib/components/MenuIcon.svelte
@@ -1,68 +1,29 @@
 <script>
-	import Fa from 'svelte-fa';
 	import { page } from '$app/stores';
-	import { 
-		faHome as sd_home, 
-		faCog as sd_config, 
-		faClipboardList as sd_processos, 
-		faUser as sd_perfil, 
-		faBell as sd_notificacoes,
-		faThumbtack as sd_avisos,
-		faArrowRight as sd_logout, 
-		faUsers as sd_usuarios } from '@fortawesome/free-solid-svg-icons';
-	//import { 
-	//	faHome as ol_home, 
-	//	faFlushed as ol_config, 
-	//	faClipboardList as ol_processos, 
-	//	faUser as ol_perfil, 
-	//	faBell as ol_notificacoes,
-	//	faThumbtack as ol_avisos,
-	//	faArrowRight as ol_logout, 
-	//	faUsers as ol_usuarios } from '@fortawesome/free-regular-svg-icons/index';
-
+	import '@fortawesome/fontawesome-free/css/fontawesome.css'
+	import '@fortawesome/fontawesome-free/css/solid.css'
 	export let href
 	let icons = {
-		home: {
-			sd: sd_home,
-			//ol: ol_home,
-		},
-		config: {
-			sd: sd_config,
-			//ol: ol_config,
-		},
-		processos: {
-			sd: sd_processos,
-			//ol: ol_processos,
-		},
-		notificacoes: {
-			sd: sd_notificacoes,
-			//ol: ol_notificacoes,
-		},
-		avisos: {
-			sd: sd_avisos,
-			//ol: ol_avisos,
-		},
-		perfil: {
-			sd: sd_perfil,
-			//ol: ol_perfil,
-		},
-		logout: {
-			sd: sd_logout,
-			//ol: sd_logout,
-		},
-		usuarios: {
-			sd: sd_usuarios,
-			//ol: sd_usuarios,
-		},
+		home: 'fa-home',
+		config: 'fa-cog',
+		processos: 'fa-clipboard-list',
+		notificacoes: 'fa-bell',
+		avisos: 'fa-thumbtack',
+		perfil: 'fa-user',
+		logout: 'fa-arrow-right-from-bracket',
+		usuarios: 'fa-users',
 	}
-	let icon_src = icons[href == '/' ? 'home' : href.split('/')[1]]
-	$: is_sd = true//$page.url.pathname === href
-	$: icon = is_sd ? icon_src.sd : icon_src.ol
-	console.log(`icon ${href} going to be ${is_sd ? 'sd' : 'ol'}`)
+	let is_home = href === '/'
+	let icon_src = icons[is_home ? 'home' : href.split('/')[1]]
+	$: is_on = is_home ? $page.url.pathname === href : $page.url.pathname.includes(href)
+	$: icon = 'fas ' + icon_src
 </script>
-<Fa {icon}
-	size='lg'
-	fw
-	primaryColor="var(--dark)"
-	scale='2'
-	/>
+<i class={icon + ' fa-2x'} 
+	style:color={is_on ? 'var(--dark)' : 'white'}
+	style:text-shadow={
+		!is_on ?
+			'-1px 0 var(--dark), \
+			  0 1px var(--dark), \
+			  1px 0 var(--dark), \
+			 0 -1px var(--dark)' 
+		: 'none'}/>
