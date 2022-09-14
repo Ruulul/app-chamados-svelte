27751457ce2c07f01c7a56920334b276f8060b3f
Ruulul <vamjunior01@gmail.com>
2022-09-14 16:09:44 -0300

    placeholder pra clareza (sugestão do Wesley)

--- src/routes/config/_ResetaSenha.svelte
+++ src/routes/config/_ResetaSenha.svelte
@@ -4,7 +4,7 @@
 </script>
 <form on:submit|preventDefault={()=>auth.resetaSenha(email)}>
     <label>
-        Resetar senha de: <input bind:value={email}/>
+        Resetar senha de: <input placeholder="email do usuário" bind:value={email}/>
     </label>
     <input class='action button' type='submit' value='Resetar'/>
 </form>
