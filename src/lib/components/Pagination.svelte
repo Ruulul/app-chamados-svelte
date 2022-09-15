2afd4a643512187e8d4eb8bdb629b9ef25c7a116
Ruulul <vamjunior01@gmail.com>
2022-09-15 10:38:39 -0300

    wrapping behaviour

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -4,37 +4,52 @@
     export let items_per_page = 10
 
     $: pages = Math.ceil(data.length/items_per_page)
-    $: pages_index = new Array(pages)
-    console.log(pages_index)
+    $: down = page > 1
+    $: up = page < pages
 </script>
 {#each data.slice((page - 1) * items_per_page, page * items_per_page) as page}
     <slot name='page' {page}/>
 {/each}
-<ul>
-    {#each pages_index as _, index}
-    <li on:click={()=>page=index+1} class:selected={page===(index + 1)}>{index + 1}</li>
-    {/each}
-</ul>
+<div>
+    <i on:click={()=>down ? page-- : page = pages} class='fas fa-arrow-left' class:last={pages > 1 && !down} class:disabled={pages===1}/>
+    <span>{page}</span>
+    <i on:click={()=>up ? page++ : page = 1} class='fas fa-arrow-right' class:last={pages > 1 && !up} class:disabled={pages===1}/>
+</div>
 <style>
-    ul {
+    div {
         display: flex;
         flex-flow: row;
         list-style: none;
         justify-content: space-between;
+        margin-top: 2em;
     }
-    li {
+    i, span {
         border: solid var(--dark);
         background-color: white;
         padding: 1rem;
         font-size: larger;;
         border-radius: 2em;
-        cursor: pointer;
         width: 1em;
         height: 1em;
         text-align: center;
     }
-    li.selected {
+    i {
+        cursor: pointer;
+    }
+    i.last,
+    i.disabled {
+        opacity: 0.5;
+        cursor: auto;
+    }
+    span {
         background-color: var(--dark);
         color: white;
     }
 </style>
+<!--
+    i.last {
+        background-color: blue;
+        color: white;
+        border: solid white;
+    }
+-->
