6d1a59896b39136191a2c9b17a76bee13ccf6bac
Ruulul <vamjunior01@gmail.com>
2022-09-19 16:30:18 -0300

    filter pendente

--- src/routes/notificacoes.svelte
+++ src/routes/notificacoes.svelte
@@ -2,7 +2,8 @@
 import ListarProcessos from "$lib/components/ListarProcessos.svelte";
 import { notifications } from "$lib/stores/cadastros";
 import { processos } from "$lib/stores/notifications";
+import { filterPendente } from "$lib/utils/utils";
 
 </script>
 
-<ListarProcessos custom_cadastros={$processos.filter(notifications.isNotRead)} />
+<ListarProcessos custom_cadastros={$processos.filter(p=>filterPendente(p)&&notifications.isNotRead(p))} />
