42a8ce5ede3e7b190385daf8787db086530e7080
Ruulul <vamjunior01@gmail.com>
2022-09-19 14:28:00 -0300

    notificações

---
+++ src/routes/notificacoes.svelte
@@ -0,0 +1,8 @@
+<script>
+import ListarProcessos from "$lib/components/ListarProcessos.svelte";
+import { notifications } from "$lib/stores/cadastros";
+import { processos } from "$lib/stores/notifications";
+
+</script>
+
+<ListarProcessos custom_cadastros={$processos.filter(notifications.isRead)} />
