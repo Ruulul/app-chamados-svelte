d5fc83005dd8c14f9ceac4f9830d7c20e23235ad
Ruulul <vamjunior01@gmail.com>
2022-09-21 16:42:08 -0300

    limpar filtros ao entrar na página de notificação

--- src/routes/notificacoes.svelte
+++ src/routes/notificacoes.svelte
@@ -3,7 +3,11 @@ import ListarProcessos from "$lib/components/ListarProcessos.svelte";
 import { notifications } from "$lib/stores/cadastros";
 import { processos } from "$lib/stores/notifications";
 import { filterPendente } from "$lib/utils/utils";
+import { onMount } from "svelte";
 
+let clear_filters = ()=>{}
+
+onMount(()=>clear_filters())
 </script>
 
-<ListarProcessos custom_cadastros={$processos.filter(p=>filterPendente(p)&&notifications.isNotRead(p))} />
+<ListarProcessos bind:clear_filters custom_cadastros={$processos.filter(p=>filterPendente(p)&&notifications.isNotRead(p))} />
