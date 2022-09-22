7f78eb60eefa990fc8a94808c511d0b7f77c1f5f
Ruulul <vamjunior01@gmail.com>
2022-09-22 14:05:33 -0300

    renamed again

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,20 @@
+<template lang="pug">
+    script.
+        export let dialog;
+        export let title_tag = 2;
+        export let title;
+    dialog.filled.container(bind:this!='{dialog}')
+        svelte:element.title(this!="{'h' + title_tag}") {title}
+            button.action.close.button(on:click!='{()=>dialog.close()}') X
+        slot
+        
+    style.
+        dialog {
+            position: relative;
+        }
+        dialog * > button.close.button {
+            position: absolute;
+            right: 1em;
+            top: 1em;
+        }
+</template>
