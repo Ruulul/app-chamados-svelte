094e27f5bd6a785b4baa17ddc7dc459cd2cdd1d4
Ruulul <vamjunior01@gmail.com>
2022-09-23 08:38:43 -0300

    melhoria de ExibeArquivo

--- src/lib/components/ExibeArquivo.svelte
+++ src/lib/components/ExibeArquivo.svelte
@@ -39,15 +39,14 @@ import Dialog from "./Dialog.svelte";
             div(class:hidden!='{data === "Não autorizado"}' style!='{geraCSS(style)}')
                 span {title} (Approx. {Math.floor(data.split(";base64,")[1].length/4 * 3 / 1024)} kB)
                     br
-                    button.action.button(on:click!='{()=>preview.showModal()}') Visualizar
+                    button.action.button(class:hidden!='{!is_image}' on:click!='{()=>preview.showModal()}') Visualizar
+                    a(class:hidden!='{is_image}' href!='{data}' download!='{title}'): button.action.button {download}
                     Dialog(title='' bind:dialog!='{preview}')
                         .file-wrapper
                             img(class:hidden!='{!is_image}' '{alt}' src!='{data}')
-                            object(class:hidden!='{data.split(";base64,")[1].length < 20 || is_image}' '{alt}' '{data}' '{title}')
-                                | Não pudemos exibir
+                            object(class:hidden!='{data.split(";base64,")[1].length < 20 || is_image}' '{alt}' '{data}' '{title}') Não pudemos exibir
                             div
-                                a.action.button(class:hidden!='{!(data && download)}' href!='{data}' download!='{title}')
-                                    | {download}
+                                a.action.button(class:hidden!='{!(data && download)}' href!='{data}' download!='{title}') {download}
                                 button.action.button(on:click!='{()=>preview.close()}') Voltar
     +if('data')
         +const('is_image = data.slice(0, 20).includes("image")')
